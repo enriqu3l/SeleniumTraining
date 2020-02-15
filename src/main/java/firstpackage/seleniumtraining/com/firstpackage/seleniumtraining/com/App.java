163537c5c303e05d20ce273a1d0b2c8bb68b1d8b
Enrique Lopez <enrique.lopez@wizeline.com>
2020-02-15 16:43:56 -0600

    First Commit
    Signed-off-by: Enrique Lopez <enrique.lopez@wizeline.com>

---
+++ src/main/java/firstpackage/seleniumtraining/com/firstpackage/seleniumtraining/com/App.java
@@ -0,0 +1,13 @@
+package firstpackage.seleniumtraining.com.firstpackage.seleniumtraining.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
